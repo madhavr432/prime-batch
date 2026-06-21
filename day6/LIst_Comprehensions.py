5d9738f0e958609776ac03e9e198be0c366e7734
madhavr432 <madhavr432@gmail.com>
2026-06-21 20:49:24 +0530

    Auto update Prime Batch on Sun 06/21/2026 20:49:24.51

---
+++ day6/LIst_Comprehensions.py
@@ -0,0 +1,6 @@
+squares = []
+
+for i in range(6):
+    squares.append(i*i)
+
+print(squares)
--- day6/exception_handling.py
+++ day6/exception_handling.py
@@ -2,4 +2,10 @@ try:
     x = int(input("enter x: "))
     ans = 10/x
 except ZeroDivisionError:
-    print(f")
+    print(f"Divide by 0 is not allowed")
+except ValueError:
+    print("please enter integer value")
+else:
+    print(f"ans = {ans}")
+finally:
+    print("End of program")

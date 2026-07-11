5fca3db7dcfd41acf7784257008a6c8b2282a4d5
madhavr432 <madhavr432@gmail.com>
2026-07-11 11:56:53 +0530

    Notebook update on 2026-07-11 1156

--- prime_push.bat
+++ prime_push.bat
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @echo off
 cd /d "C:\prime batch"
 
@@ -22,26 +21,3 @@ git push origin main
 
 :: Keep window open to see errors
 pause
-=======
-@echo off
-cd /d "C:\prime batch"
-
-:: Add notebooks only
-git add -- "*.ipynb"
-
-:: Optionally also add the automation script
-git add prime_push.bat
-
-:: Format date and time safely
-for /f "tokens=1-4 delims=/ " %%a in ("%date%") do set datestamp=%%d-%%b-%%c
-for /f "tokens=1-2 delims=:." %%a in ("%time%") do set timestamp=%%a%%b
-
-:: Commit only if staged changes exist
-git diff --cached --quiet || git commit -m "Notebook update on %datestamp% %timestamp%"
-
-:: Push to remote
-git push
-
-:: Keep window open to see errors
-pause
->>>>>>> 68caf48 (Notebook update on 2026-07-11 1156)
